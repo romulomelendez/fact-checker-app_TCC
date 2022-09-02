baa965d0e3e6cbc70eaa17823b84500a93ac03f6
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2022-09-02 16:21:49 -0300

    cleaning project

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,27 @@
-export const App: React.FC = () => <h1>Hello, World!</h1>
+import { useState } from 'react'
+
+export const App: React.FC = () => {
+
+  const [search, setSearch] = useState('')
+
+  const handleFactNews = () => {
+
+    try {
+      fetch(import.meta.env.VITE_BASE_API_URL + `${search}` + '&key=' + import.meta.env.VITE_API_KEY)
+      .then(response => response.json())
+      .then(data => console.log(data))
+    }
+    catch (err){
+      console.error(err)
+    }
+
+  }
+
+  return (
+    <>
+      <input type="text" placeholder="Search" value={ search } onChange={ e => setSearch(e.target.value) } />
+      <button onClick={ handleFactNews }>CHECK</button>
+    </>
+  )
+
+}
