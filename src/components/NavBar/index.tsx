6620ffe14af103bd3f2065fb061e3fc2397c9fcd
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2022-09-18 13:14:48 -0300

    reorganizing NavBar component

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -2,13 +2,11 @@ import { Container } from './styles'
 
 import { Link } from 'react-router-dom'
 
-export const NavBar: React.FC = () => {
+export const NavBar: React.FC = () => (
+        
+    <Container>
+        <Link to="/">Home</Link>
+        <Link to="/hotnews">Hot News</Link>
+    </Container>
 
-    return (
-        <Container>
-            <Link to="/">Home</Link>
-            <Link to="/hotnews">Hot News</Link>
-        </Container>
-    )
-
-}
+)
