d0e61a21d43721aa80acc4ad2772ea68dc5c6c07
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2023-05-06 10:29:26 -0300

    condicional rendering to show date of news claim

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -6,18 +6,20 @@ type CardProps = {
     report: NewsProps,
 }
 
-export const Card = ({ report }: CardProps) => {
-    console.log(report.newsReview[0].textualRating)
-    return (
+export const Card = ({ report }: CardProps) =>
+    (
         <CardContainer variant={report.newsReview[0].textualRating}>
             <CardHeader>
                 <CardTitle>{ report.originalNews.originalTitle }</CardTitle>
             </CardHeader>
             <CardContent>
-                <h5>Date: { report.originalNews.originalClaimDate }</h5>
-                <h5>Rating: { report.newsReview[0].textualRating }</h5>
-                <h5>Lang: { report.newsReview[0].languageCode }</h5>
+
+                {
+                    report.originalNews.originalClaimDate != ''
+                      && <h5>Date: { report.originalNews.originalClaimDate }</h5> 
+                }
+
+                <h5>Status: { report.newsReview[0].textualRating }</h5>
             </CardContent>                        
         </CardContainer>
     )
-}
